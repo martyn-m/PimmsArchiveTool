b3380ea5a1aedaadf896ffeadcb064af67bd27e1
Martyn Myers <martyn.myers@picsolve.com>
2014-08-06 00:32:16 +0100

    Settings and servers from xml, server combobox and ssh commands working OK. first stab at firebird connection failing

---
+++ PiMMS Archive Tool/ArchiveSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PimmsArchiveTool
+{
+    public class ArchiveSettings
+    {
+        public String SshUsername { get; set; }
+        public String SshPassword { get; set; }
+        public String DbUsername { get; set; }
+        public String DbPassword { get; set; }
+        public String DbPath { get; set; }
+    }
+}
