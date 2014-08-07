34519a183458305db8dbf11dadfbe4a6d9a033d3
Martyn Myers <martyn.myers@picsolve.com>
2014-08-07 15:00:02 +0100

    DB Query, display of results and find files working

---
+++ PiMMS Archive Tool/DataTypes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace PimmsArchiveTool
+{
+    // Type for list of servers as read from xml file
+    public class ServerList
+    {
+        [XmlElement("PimmsServer")]
+        public List<PimmsServer> server = new List<PimmsServer>(); 
+    }
+
+    // Type to define per-server settings
+    public class PimmsServer
+    {
+        public String Name { get; set; }
+        public String PKRD { get; set; }
+        public String IpAddress { get; set; }
+        public String VideoStorePath { get; set; }
+
+        public override string ToString()
+        {
+            String _string = Name + ", " + IpAddress + ", " + VideoStorePath;
+            return _string;
+        }
+    }
+
+    // Type for global settings
+    public class ArchiveSettings
+    {
+        public String SshUsername { get; set; }
+        public String SshPassword { get; set; }
+        public String DbUsername { get; set; }
+        public String DbPassword { get; set; }
+        public String DbPath { get; set; }
+    }
+
+    // Type for results from firebird db IMAGE_CAPTURE_REGISTER query
+    public class DbResult
+    {
+        public String PictureReference { get; set; }
+        public String RideId { get; set; }
+        public String ArtefactId { get; set; }
+        public String Status { get; set; }
+    }
+}
--- PiMMS Archive Tool/PimmsArchiveTool.xaml.cs
+++ PiMMS Archive Tool/PimmsArchiveTool.xaml.cs
@@ -35,6 +35,7 @@ namespace PimmsArchiveTool
             
             settings = ReadSettings();
             serverList = ReadServers();
+            PicRefRadio.IsChecked = true;
         }
 
         /// <summary>
@@ -79,70 +80,6 @@ namespace PimmsArchiveTool
             return settings;
         }
 
-        private void ConnectButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Set up the connection details
-            // TO DO: sanity check the input
-            connectionInfo = new PasswordConnectionInfo(currentServer.IpAddress, settings.SshUsername, settings.SshPassword);
-            
-            using (SshClient ssh = new SshClient(connectionInfo))
-            {
-                try
-                {
-                    ssh.Connect();
-                }
-                catch (System.Net.Sockets.SocketException ex)
-                {
-                    // Couldn't connect to remote host, either a wrong IP or network down
-                    DebugTextBox.AppendText(ex.ToString());
-                    DebugTextBox.ScrollToEnd();
-                }
-                catch (Renci.SshNet.Common.SshAuthenticationException ex)
-                {
-                    // Failed Authentication
-                    DebugTextBox.AppendText(ex.ToString());
-                    DebugTextBox.ScrollToEnd();
-                }
-                catch (Exception ex)
-                {
-                    // Something else went wrong
-                    DebugTextBox.AppendText(ex.ToString());
-                    DebugTextBox.ScrollToEnd();
-                }
-                
-                // TO DO: check that the command isn't blank, otherwise we get a System.ArgumentException
-                // TO DO: read up on asynchronous execution
-                SshCommand cmd = ssh.CreateCommand(CommandTextBox.Text);
-                IAsyncResult asynch = cmd.BeginExecute(delegate(IAsyncResult ar)
-                {
-                    //DebugTextBox.AppendText("Finished\n");
-                    //DebugTextBox.ScrollToEnd();
-                }, null);
-
-                StreamReader reader = new StreamReader(cmd.OutputStream);
-
-                while (!asynch.IsCompleted)
-                {
-                    String result = reader.ReadToEnd();
-                    if (string.IsNullOrEmpty(result))
-                        continue;
-                    DebugTextBox.AppendText(result);
-                    DebugTextBox.ScrollToEnd();
-                }
-                cmd.EndExecute(asynch);
-            }
-        }
-
-        private void ReadSettingsButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void ReadServersButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void ServerComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             currentServer = serverList.server[ServerComboBox.SelectedIndex];
@@ -157,16 +94,21 @@ namespace PimmsArchiveTool
             ServerComboBox.SelectedIndex = 0;
         }
 
-        private void CommandTextBox_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Return)
-            {
-                ConnectButton_Click(sender, e);
-            }
-        }
-
         private void DbQueryButton_Click(object sender, RoutedEventArgs e)
         {
+            // Build the DB query
+            String dbQuery = "SELECT r.PICTUREREFERENCE, r.RIDEID, r.ARTEFACTID, r.VIDEOSTATUS " +
+                            "FROM IMAGE_CAPTURE_REGISTER r ";
+            if (PicRefRadio.IsChecked == true)
+            {
+                dbQuery += "WHERE r.PICTUREREFERENCE containing '" + GetQueryPicRef() + "'";
+            }
+            else
+            {
+                dbQuery += "WHERE r.PICTUREREFERENCE CONTAINING '" + GetQueryId() +
+                            "' AND r.PICTUREREFERENCE CONTAINING '" + GetQueryDate() + "'";  
+            }
+            
             // Create a connection string using the stored DB details and the current server IP
             FbConnectionStringBuilder builder = new FbConnectionStringBuilder();
             builder.DataSource = currentServer.IpAddress;
@@ -179,23 +121,178 @@ namespace PimmsArchiveTool
             try
             {
                 connection.Open();
-           
-                FbCommand readCommand = new FbCommand(
-                        "SELECT r.ID, r.SSID, r.ENCRYPTION, r.WIFIKEY, r.IPADDRESS" +
-                        " FROM AP_CONFIG r", connection);
+
+                DebugTextBox.AppendText(dbQuery + "\n");
+                
+                FbCommand readCommand = new FbCommand(dbQuery, connection);
+
                 using (FbDataReader reader = readCommand.ExecuteReader())
                 {
+                    List<DbResult> results = new List<DbResult>();
                     while (reader.Read())
                     {
-                        DebugTextBox.AppendText(reader[1].ToString());
-                        DebugTextBox.ScrollToEnd();
+                        results.Add(new DbResult() {
+                                PictureReference = reader.GetString(0), 
+                                RideId = reader.GetString(1),
+                                ArtefactId = reader.GetString(2), 
+                                Status = reader.GetString(3)
+                        });
                     }
+                    
+                    // Push the results to the datagrid
+                    DbQueryDataGrid.ItemsSource = results;
+                    DbQueryDataGrid.SelectedIndex = 0;
                 }
             }
             catch (Exception ex)
             {
                 DebugTextBox.AppendText(ex.ToString());
                 DebugTextBox.ScrollToEnd();
+            }
+        }
+
+        private string GetQueryPicRef()
+        {
+            // TO DO: check for SQL injection nastiness
+            return PicRefTextBox.Text;
+        }
+
+        private string GetQueryId()
+        {
+            int picId = 0;
+
+            // check that text contains only an integer number
+            try
+            {
+                picId = Int32.Parse(PicIdTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter an integer Picture ID");
+            }
+
+            // pad to 4 characters and return
+            return picId.ToString("d4");
+        }
+
+        private string GetQueryDate()
+        {
+            DateTime? date = PicIdDatePicker.SelectedDate;
+            // TO DO: catch null date
+
+            String queryDate = date.Value.Month.ToString("X") +
+                date.Value.Day.ToString("d2");
+             
+            return queryDate;
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            DbResult result = (DbResult)DbQueryDataGrid.SelectedItem;
+            
+
+            try
+            {
+                String searchCommand = "ls -1 " + currentServer.VideoStorePath + "/Ride." +
+                                    result.RideId + "." + result.ArtefactId + ".*";
+
+                DebugTextBox.AppendText(searchCommand + "\n");
+                
+                // Set up the connection details
+                // TO DO: sanity check the input
+                connectionInfo = new PasswordConnectionInfo(currentServer.IpAddress, settings.SshUsername, settings.SshPassword);
+
+                using (SshClient ssh = new SshClient(connectionInfo))
+                {
+                    try
+                    {
+                        ssh.Connect();
+                    }
+                    catch (System.Net.Sockets.SocketException ex)
+                    {
+                        // Couldn't connect to remote host, either a wrong IP or network down
+                        DebugTextBox.AppendText(ex.ToString());
+                        DebugTextBox.ScrollToEnd();
+                    }
+                    catch (Renci.SshNet.Common.SshAuthenticationException ex)
+                    {
+                        // Failed Authentication
+                        DebugTextBox.AppendText(ex.ToString());
+                        DebugTextBox.ScrollToEnd();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Something else went wrong
+                        DebugTextBox.AppendText(ex.ToString());
+                        DebugTextBox.ScrollToEnd();
+                    }
+
+                    // TO DO: check that the command isn't blank, otherwise we get a System.ArgumentException
+                    // TO DO: read up on asynchronous execution
+                    SshCommand cmd = ssh.CreateCommand(searchCommand);
+                    IAsyncResult asynch = cmd.BeginExecute(delegate(IAsyncResult ar)
+                    {
+                        //DebugTextBox.AppendText("Finished\n");
+                        //DebugTextBox.ScrollToEnd();
+                    }, null);
+
+                    StreamReader reader = new StreamReader(cmd.OutputStream);
+                    List<String> files = new List<String>();
+
+                    while (!asynch.IsCompleted)
+                    {
+                        String sshOutput;
+                        while ((sshOutput = reader.ReadLine()) != null)
+                        {
+                            files.Add(sshOutput);
+                        }
+                    }
+                    cmd.EndExecute(asynch);
+                    FilesListBox.ItemsSource = files;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                // No results were selected
+                MessageBox.Show("Please select a query result");
+            }
+            
+        }
+
+        private void PicRefRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            // Enable the picref textbox
+            PicRefTextBox.IsEnabled = true;
+            // Disable the pic id textbox and date picker
+            PicIdTextBox.IsEnabled = false;
+            PicIdDatePicker.IsEnabled = false;
+            //DebugTextBox.AppendText("PicRefRadio_Checked\n");
+        }
+
+        private void DateIdRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            // Enable the pic id / date controls
+            PicIdDatePicker.IsEnabled = true;
+            PicIdTextBox.IsEnabled = true;
+            // Disable the picref textbox
+            PicRefTextBox.IsEnabled = false;
+        }
+
+        // Run DB Query if enter is pressed in the picture reference text box
+        private void PicRefTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                DbQueryButton_Click(sender, e);
+            }
+        }
+
+        // Run DB Query if enter is pressed in the picture reference text box
+        private void PicIdTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                DbQueryButton_Click(sender, e);
             }
         }
     }
